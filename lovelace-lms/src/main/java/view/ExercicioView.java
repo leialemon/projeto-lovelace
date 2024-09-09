31ac038c2c9ef7cee307b9886285e5d208a3c083
leialemon <julianabarrosf@protonmail.com>
2024-09-09 09:26:36 -0300

    Criação da estrutura de classes View

--- lovelace-lms/src/main/java/view/ExercicioView.java
+++ lovelace-lms/src/main/java/view/ExercicioView.java
@@ -1,10 +1,18 @@
 package view;
 
+import java.util.List;
+
 import model.Exercicio;
+import model.QuestaoImpl;
 
 public class ExercicioView {
     public static void mostrarExercicio(Exercicio exercicio){
-        
+        List<QuestaoImpl> questoes = exercicio.getQuestoes();
+        System.out.println("\n### Exercício ###\n");
+        int i = 0;
+        for (QuestaoImpl q : questoes){
+            QuestaoView.mostrarQuestao(i++, q);
+            System.out.println();
+        }
     }
 }
-// mostrar as questões em ordem aleatória;
