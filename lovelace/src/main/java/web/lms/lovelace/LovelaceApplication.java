bd5fe317df79a0c1b755e1d72b3fb4a3054388a2
leialemon <julianabarrosf@protonmail.com>
2024-09-18 09:12:39 -0300

    criando aplicação web com spring boot

---
+++ lovelace/src/main/java/web/lms/lovelace/LovelaceApplication.java
@@ -0,0 +1,13 @@
+package web.lms.lovelace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovelaceApplication.class, args);
+	}
+
+}
---
+++ lovelace/src/test/java/web/lms/lovelace/LovelaceApplicationTests.java
@@ -0,0 +1,13 @@
+package web.lms.lovelace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
